1aba99f559b33528654b0093ea5409753335e15e
Alex Mattingley <simplyalexgreen@gmail.com>
2024-01-02 22:46:02 +0000

    refactor footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,41 +1,46 @@
+import styles from "@/styles/Footer.module.css";
+
 function Footer() {
   return (
     <div
       name="Footer-container"
-      style={{
-        width: "99.1vw",
-        height: "20vh",
-        backgroundColor: "#48021e",
-        display: "flex",
-        justifyContent: "center",
-      }}
+      className={styles.footerContainer}
+      // style={{
+      //   width: "99.1vw",
+      //   height: "20vh",
+      //   backgroundColor: "#48021e",
+      //   display: "flex",
+      //   justifyContent: "center",
+      // }}
     >
       <div
         name="Footer-content-container"
-        style={{
-          width: "66vw",
-          height: "100%",
-          backgroundColor: "translucent",
-          display: "flex",
-          justifyContent: "flex-start",
-        }}
+        className={styles.footerContentContainer}
+        // style={{
+        //   width: "66vw",
+        //   height: "100%",
+        //   backgroundColor: "translucent",
+        //   display: "flex",
+        //   justifyContent: "flex-start",
+        // }}
       >
         <div
           name="Text-footer-div"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignContent: "center",
-            backgroundColor: "translucent",
-            width: "32vw",
-            height: "20vh",
-            color: "white",
-            fontFamily: "EB Garamond",
-            fontSize: "0.75rem",
-            fontStyle: "normal",
-            fontWeight: "500",
-          }}
+          className={styles.textFooterDiv}
+          // style={{
+          //   display: "flex",
+          //   flexDirection: "column",
+          //   justifyContent: "center",
+          //   alignContent: "center",
+          //   backgroundColor: "translucent",
+          //   width: "32vw",
+          //   height: "20vh",
+          //   color: "white",
+          //   fontFamily: "EB Garamond",
+          //   fontSize: "0.75rem",
+          //   fontStyle: "normal",
+          //   fontWeight: "500",
+          // }}
         >
           <p>HILLTOP MANOR, TREMADOG, PORTHMADOG LL49 9RG</p>
           <p>TELEPHONE: +44 (0) 1766 514 682</p>
@@ -60,16 +65,17 @@ function Footer() {
           </div> */}
         <div
           name="socials-div"
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            backgroundColor: "translucent",
-            width: "100%",
-            height: "100%",
-            cursor: "pointer",
-          }}
+          className={styles.socialsDiv}
+          // style={{
+          //   display: "flex",
+          //   flexDirection: "row",
+          //   justifyContent: "flex-end",
+          //   alignItems: "center",
+          //   backgroundColor: "translucent",
+          //   width: "100%",
+          //   height: "100%",
+          //   cursor: "pointer",
+          // }}
         >
           <img
             src="images/Socials.png"
